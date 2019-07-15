2fb0692b2223cc82f96b781ce51342cec29a8fed
Shakar Saleem <36695899+ShakarSaleem@users.noreply.github.com>
2019-07-15 10:01:57 +0100

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -1,14 +1,13 @@
 <?php
     //connect to the server
 	$servername = "localhost";
-	$server_username = "root";
-	$server_password = "password";
-	$dbName = "switchdata";
-    //port variables
-	$p1 = $_POST["p1Post"];//0;
-	$p2 = $_POST["p2Post"];//0;
-	$p3 = $_POST["p3Post"];//0;
-	$p4 = $_POST["p4Post"];//0;
+	$server_username = "NAME OF USER FOR db";
+	$server_password = "PASSWORD OF DB";
+	$dbName = "NAME OF THE DB";
+    //NAMES OF THE COLUMNS
+	$NAMEOFCOLUMN1=$_POST["NAMEOFCOLUMN1"];
+	$NAMEOPFCOLUMN2=$_POST["NAMEOFCOLUMN2"];
+...
 	//connect to database
 	$conn = new mysqli($servername, $server_username, $server_password, $dbName);
 	//cheack connection
@@ -17,8 +16,8 @@
 
 	}
 	//insert into database
-	$sql = "INSERT INTO switchTable (p1,p2,p3,p4)
-	VALUES ('".$p1."','".$p2."','".$p3."','".$p4."')";
+	$sql = "INSERT INTO switchTable (NAMEOFCOLUMN1,NAMEOFCOLUMN2...)
+	VALUES ('".$NAMEOFCOLUMN1."','".$NAMEOFCOLUMN2."', ... ')";
 	$result = mysqli_query($conn ,$sql);
     //cheack if it works (in url ip then / name of this file .php)
 	if(!result) echo "there was an error";
